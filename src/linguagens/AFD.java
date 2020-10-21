a116470e5a7081fa7039296960b7f8c320c22152
marcoajt <marcoantoniojt@gmail.com>
2020-10-21 18:52:39 -0300

    Funções reconhecedor

--- src/linguagens/AFD.java
+++ src/linguagens/AFD.java
@@ -25,10 +25,130 @@ public class AFD {
     private boolean rec;
       
                     // DADOS INSERIDOS PELO USUARIO
+    public void iniciaAFD(){
+        System.out.println("Digite o alfabeto?");
+        this.alfabeto = in.nextLine();
+        this.alf = this.alfabeto.toCharArray();
+               
+                    // ESTADO INICIAL EX: A
+        System.out.println("Digite o estado inicial?");
+        this.estinicial = in.nextLine();
+        this.ini = this.estinicial.charAt(i);
+        this.atual = this.ini;
+                    // ESTADO FINAL PODE SER MAIS DE UM EX: BC
+        System.out.println("Digite o estado final?");
+        estfinal = in.nextLine();
+        estfim = estfinal.toCharArray();
+                    // FUNÇOES DE TRANSIÇÃO DE ESTADOS
+                    // EX: A0B    ESTADO ATUAL: A  VALOR DA TRANSIÇÃO: 0 PROX. ESTADO: B
+        System.out.println("Digite as transiçoes de estado separando por ',' uma da outra?");
+        funcoes = in.nextLine();
+               
+        String transicao[] = funcoes.split(",");
+                // PALAVRA PARA TESTAR O AUTOMATO
+        System.out.println("Digite a palavra para testar o automato?");
+        palavra = in.nextLine();
+        pal = palavra.toCharArray();
+        
+        organiza(transicao);
+        reconhecedor();
+    }
+    
+    
+    public void organiza(String transicao[]){
+         // ATRIBUI AS FUNÇÕES DE TRANSIÇÕES EM UMA MATRIZ 
+               while(j<transicao.length){ // CONTADOR PARA SABER QUANTAS TRANSIÇÕES O USUARIO INSERIU 
+               j++;
+               }
+
+                mat = new char [j][3];
+               // PERCORRE CARA TRANSIÇÃO PARA INSERIR NA MATRIZ
+               while(cont< transicao.length){
+                  
+                   mat[cont][0] = transicao[cont].charAt(0);
+                   mat[cont][1] = transicao[cont].charAt(1);
+                   mat[cont][2] = transicao[cont].charAt(2);
+          
+                 System.out.println("Valores na matriz: " +mat[cont][0] +mat[cont][1] +mat[cont][2]);
+                
+                    cont++;
+                   }
+                
+               cont=0;  
+    }
+    
+    public void reconhecedor(){
+        cont = 0;
+        // VERIFICA O TAMANHO DA PALAVRA PARA SER TESTADA
+               while(cont < pal.length){
+               
+                   aux=0;
+                   // LOGICA APLICADA PARA FAZER A TRANSIÇÃO DE UM ESTADO PARA O OUTRO
+                   while( aux != j) {
+                       
+                       if(mat[aux][1]== pal[cont]){
+                        if(mat[aux][0]== atual){
+                            atual = mat[aux][2];
+                            aux = j-1;
+                        }
+                       }
+                       
+                       aux++;
+                       
+                   }
+                
+                   System.out.println("Estado atual: " +atual);
+                   
+               
+               cont++;
+               
+               
+               }
+
+               
+
+                   System.out.println("Automato finalizado!!!");
+                   System.out.println("Estado final: " +atual);
+                   cont=0;
+                   
+                   
+                   // VERIFICA SE EXISTE MAIS DE UM FINAL DIGITADO PELO USUARIO
+                   while(cont!=estfim.length){
+                       
+                       if(atual == estfim[cont]){
+                        fim = estfim[cont];
+                       }
+                       
+                       System.out.println("Teste de laço: "+ estfim[cont]);
+                       cont++;
+                   }
+  
+                   System.out.println("Estado final: " +fim);
+                   // MOSTRA SE FOI RECONHECIDA A PALAVRA OU NAO 
+                   
+                   rec = Reconhecer(atual,fim);
+                   if(rec = true){
+                    System.out.println("Palavra reconhecida pelo automato!!!");
+                  }else{
+                       System.out.println("Palavra rejeitada pelo automato!!!");
+                 }
+        
+    }
+    
+            boolean Reconhecer(char atual,char fim){
+                 boolean R;
+                 if(atual == fim){
+                     return true;
+                 }else{
+                     return false;
+                 }
+             }
+    
+    
 
             
                     
-             public void iniciaAFD(){
+             /*public void iniciaAFD(){
 
                 System.out.println("Digite o alfabeto?");
                this.alfabeto = in.nextLine();
@@ -130,14 +250,7 @@ public class AFD {
                  }
 
 
-             }
+             }*/
              
-             boolean Reconhecer(char atual,char fim){
-                 boolean R;
-                 if(atual == fim){
-                     return true;
-                 }else{
-                     return false;
-                 }
-             }
+
 }
